3f449f1d3ae1cfaebf7435b9412eb2f618cec386
Trần Ka Bun <145275811+bunwg29@users.noreply.github.com>
2023-11-21 19:49:31 +0700

    Delete UserInterface.java

--- UserInterface.java
+++
@@ -1,6 +0,0 @@
-public interface UserInterface {
-    void deposit(double amount);
-    void transfer(double amount, User recipient, String recipientAccountId);
-    void withdraw(double amount);
-    double checkBalance();
-}
